ad1beb985647cc998406f73108720b78fabcb3cf
Felipe Rubio <juan.feliperubiog@gmail.com>
2024-05-27 00:01:11 -0500

    Project creation

---
+++ MeLi Challenge/MeLi Challenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MeLi Challenge
+//
+//  Created by Felipe on 26/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MeLi Challenge/MeLi Challenge/MeLi_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeLi_ChallengeApp.swift
+//  MeLi Challenge
+//
+//  Created by Felipe on 26/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MeLi_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
