7c3fc8c1831c5c3e4655a4ea871d98dbbe459ffd
Felipe Rubio <juan.feliperubiog@gmail.com>
2024-05-28 04:35:17 -0500

    documentation for product service

--- MeLi Challenge/MeLi Challenge/Services/ProductServiceProtocol.swift
+++ MeLi Challenge/MeLi Challenge/Services/ProductServiceProtocol.swift
@@ -5,6 +5,11 @@
 //  Created by Felipe on 28/05/24.
 //
 
+/// A protocol defining the services related to products.
 public protocol ProductServiceProtocol {
+    /// Searches for products based on a query.
+    ///
+    /// - Parameter query: The search query string.
+    /// - Returns: An array of `Product` that match the query.
     func searchProductsBy(_ query: String) -> [Product]
 }
