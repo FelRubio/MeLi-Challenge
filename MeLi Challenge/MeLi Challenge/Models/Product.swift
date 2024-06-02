a2d728396b51613d778e0db4a1c796215095cce9
Felipe Rubio <juan.feliperubiog@gmail.com>
2024-06-02 07:36:18 -0500

    Update models

--- MeLi Challenge/MeLi Challenge/Models/Product.swift
+++ MeLi Challenge/MeLi Challenge/Models/Product.swift
@@ -17,24 +17,18 @@ public struct Product: Identifiable, Hashable {
     public let thumbnailURL: String
     /// The price of the product.
     public let price: Int
-    /// The attributes of the product.
-    public let attributes: [ProductAttribute]
+    /// The original price of the product (optional).
+    public let originalPrice: Int?
     /// The currency ID for the product price.
     public let currencyId: String
     /// The condition of the product (e.g., new, used).
     public let condition: String
-    /// The name of the seller.
-    public let seller: String
     /// The quantity of the product available.
     public let quantityAvailable: Int
     /// The shipping data of the product.
     public let shippingData: ShippingData
     /// The installment payment data of the product.
     public let installmentsData: InstallmentsData
-    /// The description of the product.
-    public let description: String
-    /// The images URL of the product to be displayed.
-    public let images: [URL]
 
     /// Initializes a new instance of `Product`.
     /// - Parameters:
@@ -42,42 +36,33 @@ public struct Product: Identifiable, Hashable {
     ///   - title: The title of the product.
     ///   - thumbnailURL: The URL of the product's thumbnail image.
     ///   - price: The price of the product.
-    ///   - attributes: The attributes of the product.
+    ///   - originalPrice: The original price of the product (optional).
     ///   - currencyId: The currency ID for the product price.
     ///   - condition: The condition of the product (e.g., new, used).
-    ///   - seller: The name of the seller.
     ///   - quantityAvailable: The quantity of the product available.
     ///   - shippingData: The shipping data of the product.
     ///   - installmentsData: The installment payment data of the product.
-    ///   - description: The description of the product.
-    ///   - images: The array of images URL of the product to be displayed.
     public init(
         id: String,
         title: String,
         thumbnailURL: String,
         price: Int,
-        attributes: [ProductAttribute],
+        originalPrice: Int?,
         currencyId: String,
         condition: String,
-        seller: String,
         quantityAvailable: Int,
         shippingData: ShippingData,
-        installmentsData: InstallmentsData,
-        description: String,
-        images: [URL]
+        installmentsData: InstallmentsData
     ) {
         self.id = id
         self.title = title
         self.thumbnailURL = thumbnailURL
         self.price = price
-        self.attributes = attributes
+        self.originalPrice = originalPrice
         self.currencyId = currencyId
         self.condition = condition
-        self.seller = seller
         self.quantityAvailable = quantityAvailable
         self.shippingData = shippingData
         self.installmentsData = installmentsData
-        self.description = description
-        self.images = images
     }
 }
--- MeLi Challenge/MeLi Challenge/Models/ProductDetail.swift
+++ MeLi Challenge/MeLi Challenge/Models/ProductDetail.swift
@@ -11,19 +11,27 @@ import Foundation
 public struct ProductDetail: Identifiable, Hashable {
     /// The unique identifier of the product detail.
     public let id: String
+    public let name: String
     /// The attributes of the product.
     public let attributes: [ProductAttribute]
     /// The URLs of the product's pictures.
     public let pictures: [URL]
+    public let warranty: String
+    public let description: String
+    public let condition: String
     
     /// Initializes a new instance of `ProductDetail`.
     /// - Parameters:
     ///   - id: The unique identifier of the product detail.
     ///   - attributes: The attributes of the product.
     ///   - pictures: The URLs of the product's pictures.
-    public init(id: String, attributes: [ProductAttribute], pictures: [URL]) {
+    public init(id: String, name: String, attributes: [ProductAttribute], pictures: [URL], warranty: String, description: String, condition: String) {
         self.id = id
+        self.name = name
         self.attributes = attributes
         self.pictures = pictures
+        self.warranty = warranty
+        self.description = description
+        self.condition = condition
     }
 }
