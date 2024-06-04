0a097df3ca08f6d4ec3bd3956320d5ed0ce1eac4
Felipe Rubio <juan.feliperubiog@gmail.com>
2024-06-04 16:01:11 -0500

    Debug logs for repository

--- MeLi Challenge/MeLi Challenge/Repositories/ProductRepository.swift
+++ MeLi Challenge/MeLi Challenge/Repositories/ProductRepository.swift
@@ -10,6 +10,7 @@ public class ProductRepository: ProductRepositoryProtocol {
 
     public func fetchProductsBy(_ query: String) async throws -> [Product] {
         do {
+            Logger.log("Fetching products by query: \(query)", level: .info)
             let url = APIEndpoints.searchProducts.buildEndpointWith(baseURL: "https://api.mercadolibre.com/")
             let result: FetchProductsResults = try await apiClient.request(
                 url: url,
@@ -19,37 +20,48 @@ public class ProductRepository: ProductRepositoryProtocol {
                     .init(name: "q", value: query)
                 ]
             )
-            return result.results.elements.compactMap {
+            let results = result.results.elements.compactMap {
                 ProductTranslator.translate(from: $0)
             }
+            Logger.log("\(results.count) products found!", level: .info)
+            return results
         } catch {
+            Logger.log("Could not fetch products by query: \(query), Error: \(error.localizedDescription)", level: .info)
             throw ProductError.couldNotFecthProducts(error)
         }
     }
     
     public func getProductDetailWith(productIdentifier: String) async throws -> ProductDetail? {
         do {
+            Logger.log("Getting product details with product Id: \(productIdentifier)", level: .info)
             let url = APIEndpoints.fetchProduct.buildEndpointWith(baseURL: "https://api.mercadolibre.com/") + productIdentifier
             let result: ProductDetailDTO = try await apiClient.request(url: url, method: .get)
-            let description = try await getProductDescription(with: productIdentifier)
-            return ProductDetailTranslator.translate(from: result, with: description)
+            let description = try? await getProductDescription(with: productIdentifier)
+            let detail = ProductDetailTranslator.translate(from: result, with: description ?? "")
+            Logger.log("Details found for product \(productIdentifier)", level: .info)
+            return detail
         } catch {
+            Logger.log("Could not get product: \(productIdentifier) detail, Error: \(error.localizedDescription)", level: .info)
             throw ProductError.couldNotGetDetailsForProduct(productIdentifier, error)
         }
     }
     
     public func getProductDescription(with id: String) async throws -> String {
         do {
+            Logger.log("Getting product description with product Id: \(id)", level: .info)
             let url = APIEndpoints.fetchProduct.buildEndpointWith(baseURL: "https://api.mercadolibre.com/") + "\(id)/" + APIEndpoints.description.path
             let result: DescriptionDTO = try await apiClient.request(url: url, method: .get)
+            Logger.log("Description found for product \(id)", level: .info)
             return result.text
         } catch {
+            Logger.log("Could not get description for product: \(id) detail, Error: \(error.localizedDescription)", level: .info)
             throw ProductError.couldNotGetDescriptionForProduct(id, error)
         }
     }
     
     public func fetchPromotions() async throws -> [Product] {
         do {
+            Logger.log("Fetching promotion products...", level: .info)
             let url = APIEndpoints.searchProducts.buildEndpointWith(baseURL: "https://api.mercadolibre.com/")
             let result: FetchProductsResults = try await apiClient.request(
                 url: url,
@@ -59,10 +71,13 @@ public class ProductRepository: ProductRepositoryProtocol {
                     .init(name: "q", value: "promotions")
                 ]
             )
-            return result.results.elements.compactMap {
+            let results = result.results.elements.compactMap {
                 ProductTranslator.translate(from: $0)
             }
+            Logger.log("\(results.count) products found!", level: .info)
+            return results
         } catch {
+            Logger.log("Could not fetch promotion products, Error: \(error.localizedDescription)", level: .info)
             throw ProductError.couldNotGetPromotions(error)
         }
     }
